62db38cb2ea376578ae5d43855566a430fa62e00
marcello-carrasola-91 <marcello.carrasola@gmail.com>
2025-03-05 00:25:13 -0300

    Update ScreenshotUtil.java

--- src/test/java/cucumber/util/ScreenshotUtil.java
+++ src/test/java/cucumber/util/ScreenshotUtil.java
@@ -1,14 +1,6 @@
 package cucumber.util;
 
-
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
-import io.cucumber.java.Scenario;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
 
 public class ScreenshotUtil {
 
